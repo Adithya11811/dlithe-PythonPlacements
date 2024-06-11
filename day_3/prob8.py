38266383fcfbd834bcc1ea53bf2302c109f491ee
Adithya Rao K <116443929+Adithya11811@users.noreply.github.com>
2024-06-11 10:45:33 +0530

    minor fix: day-3

--- day_3/prob8.py
+++ day_3/prob8.py
@@ -2,14 +2,26 @@
 
 li = [-11,2,2,-3, 4,12,-13]
 li=[1,2,3,4]
-maxi=0
-n = len(li)+1
-for subarr in range(1, n+1):
-    for i in range(0, n-subarr+1):
 
-        res = li[i: i+subarr]
-        val =sum(res)
-        if maxi < val:
-            maxi =val
+# method 1
+def largest_subArr_sum(lst):
+    max_sum= cur_sum = lst[0]
+    for num in lst[1:]:
+        cur_sum = max(num, cur_sum+num)
+        max_sum = max(cur_sum, max_sum)
 
-print(maxi)
+    return max_sum
+
+print(largest_subArr_sum(li))
+# method 2
+# maxi=0
+# n = len(li)+1
+# for subarr in range(1, n+1):
+#     for i in range(0, n-subarr+1):
+
+#         res = li[i: i+subarr]
+#         val =sum(res)
+#         if maxi < val:
+#             maxi =val
+
+# print(maxi)
